afe30c4f5e73bb4e8e24ec74dbc1f38977545f99
Roy Mejia <roymejia2217@users.noreply.github.com>
2026-04-23 18:38:34 -0500

    fix(utils): replace hardcoded SVG tooltip icon with semantic abbr element
    Replace inline SVG path data for the quality parameter help icon with
    a native <abbr> element styled via Bootstrap utilities. <abbr> is
    semantic HTML for abbreviations and explanations, eliminating hardcoded
    vector graphics while preserving tooltip functionality.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -253,11 +253,7 @@ const utils = {
     if (TOOLTIP_DESCRIPTIONS[tooltipKey]) {
       label.innerHTML = `
         ${this.sanitizeText(option.label)}
-        <span class="d-inline-flex" data-bs-toggle="tooltip" data-bs-placement="top" title="${this.sanitizeText(TOOLTIP_DESCRIPTIONS[tooltipKey])}">
-          <svg class="flex-shrink-0" width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-          </svg>
-        </span>
+        <abbr class="d-inline-flex text-secondary text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="top" title="${this.sanitizeText(TOOLTIP_DESCRIPTIONS[tooltipKey])}" style="cursor: help;">?</abbr>
       `;
       if (typeof bootstrap !== 'undefined' && bootstrap.Tooltip) {
         const tooltipTrigger = label.querySelector('[data-bs-toggle="tooltip"]');
